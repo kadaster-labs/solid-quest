c0fb233f72b45bed0185d0aa4577f0793f4d4a28
Marc van Andel <2315396+marcvanandel@users.noreply.github.com>
2023-02-09 16:25:03 +0100

    Building koopovereenkomst with LDFLex (#41)
    * fix: LDFLex example for koopovereenkomst
    * reading from events and building the aggregate
    * calling kkg + building aggregate data
    * asynchronous processing of events
    * start solid-pod-provider only (by default during development)
    -----
    Co-authored-by: Stefan Bussemaker <stefan.bussemaker@kadaster.nl>

---
+++ koopovereenkomst-v3-simple/aggregate/context.ts
@@ -0,0 +1,38 @@
+export const GENERAL_CONTEXT = {
+  rdf: "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+  rdfs: "http://www.w3.org/2000/01/rdf-schema#",
+  prov: "https://www.w3.org/TR/prov-o/#",
+};
+
+export const SOLID_ZVG_CONTEXT = {
+  ...GENERAL_CONTEXT,
+  cloudevents: "https://cloudevents.io/def/",
+
+  zvg: "http://taxonomie.zorgeloosvastgoed.nl/def/zvg#",
+
+  type: "rdf:type",
+  label: "rdfs:label",
+  wasGeneratedBy: "prov:wasGeneratedBy",
+  sequence: "cloudevents:sequence",
+  eventData: "cloudevents:data",
+  time: "cloudevents:time",
+
+  aan: "zvg:aan",
+  aangebodenDoor: "zvg:aangebodenDoor",
+  koopprijs: "zvg:koopprijs",
+  datumVanLevering: "zvg:datumVanLevering",
+  bod: "zvg:bod",
+  template: "zvg:koopovereenkomstTemplate",
+  kadastraalObjectId: "zvg:kadastraalObjectId",
+};
+
+export const KADASTER_KKG_CONTEXT = {
+  "@context": {
+    sor: "https://data.kkg.kadaster.nl/sor/model/def/",
+    perceel: "https://data.kkg.kadaster.nl/id/perceel/",
+    perceelnummer: {
+      "@id": "sor:perceelnummer",
+      "@type": "xsd:integer",
+    },
+  },
+};
--- koopovereenkomst-v3-simple/components/connect.tsx
+++ koopovereenkomst-v3-simple/components/connect.tsx
@@ -5,12 +5,16 @@ import styles from '../styles/connect.module.css';
 export default function ConnectSolid() {
 
     const { session, sessionRequestInProgress } = useSession();
+    const webId = session.info.webId;
 
     return (
         <div className={styles.connectBox}>
             <h2>Connect to Solid POD</h2>
             {!sessionRequestInProgress && session.info.isLoggedIn && (
-                <LogoutButton onError={console.error} onLogout={() => window.location.reload()}></LogoutButton>
+                <span>
+                    <LogoutButton onError={console.error} onLogout={() => window.location.reload()}></LogoutButton>
+                    <a href={webId} target="_blank" rel="noreferrer">Open WebID</a>
+                </span>
             )}
 
             {!sessionRequestInProgress && !session.info.isLoggedIn && <LoginForm />}
