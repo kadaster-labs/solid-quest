f3761204746e7dfc90685f43944e82f678a4c2d1
marcvanandel <marc.vanandel@kadaster.nl>
2023-03-02 17:31:53 +0100

    remove console log

--- koopovereenkomst-v3-simple/src/theme.ts
+++ koopovereenkomst-v3-simple/src/theme.ts
@@ -47,12 +47,7 @@ export const commonTokens = {
 };
 
 export const buildTheme = (mode: PaletteMode) => {
-  //   let t = {
-  //       ...getDesignTokens(mode),
-  //     ...commonTokens,
-  //   };
-  let t = merge(commonTokens, getDesignTokens(mode));
-  console.log(t);
+  let t = deepmerge(commonTokens, getDesignTokens(mode));
   return t;
 };
 
@@ -75,11 +70,11 @@ const getDesignTokens = (mode: PaletteMode) => ({
 });
 
 // Merge a `source` object to a `target` recursively
-const merge = (target, source) => {
+const deepmerge = (target, source) => {
   // Iterate through `source` properties and if an `Object` set property to merge of `target` and `source` properties
   for (const key of Object.keys(source)) {
     if (source[key] instanceof Object)
-      Object.assign(source[key], merge(target[key], source[key]));
+      Object.assign(source[key], deepmerge(target[key], source[key]));
   }
 
   // Join `target` and modified `source`
