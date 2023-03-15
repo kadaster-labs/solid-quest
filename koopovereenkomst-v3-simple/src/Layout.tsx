824458df103cab3ff6297b923252ea55f07f1a91
Marc van Andel <marc.vanandel@kadaster.nl>
2023-03-15 15:16:55 +0100

    css to ts + clean up

--- koopovereenkomst-v3-simple/src/Layout.tsx
+++ koopovereenkomst-v3-simple/src/Layout.tsx
@@ -1,10 +1,9 @@
 import { SessionProvider } from "@inrupt/solid-ui-react";
-import { Typography } from "@mui/material";
+import { Typography, useMediaQuery } from "@mui/material";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Head from 'next/head';
 import { useEffect, useState } from "react";
-import styles from '../styles/layout.module.css';
 import Image from './Image';
 import Link from "./Link";
 
@@ -16,6 +15,34 @@ export default function Layout({ children, home, role }: {
     role?: string,
 }) {
     const [currentTab, setCurrentTab] = useState(0);
+
+    const minimalWidth = useMediaQuery('only screen and (max-width: 1500px)');
+
+    const footerStyle = {
+        display: "flex",
+        flex: 1,
+        padding: "1rem 0",
+        borderTop: "1px solid #ccc",
+        justifyContent: "center",
+        alignItems: "center",
+        position: "fixed",
+        left: 0,
+        bottom: 0,
+        width: "100%",
+        background: "rgb(0, 44, 84)",
+        color: "white",
+        textAlign: "center",
+        a: {
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            color: "white",
+            margin: "0 1rem",
+            '&:hover': {
+                textDecoration: "none",
+            }
+        }
+    }
 
     useEffect(() => {
         switch (role) {
@@ -34,11 +61,23 @@ export default function Layout({ children, home, role }: {
     }, [role]);
 
     return (
-        <Box className={styles.backgroundLines}>
+        <Box sx={{
+            resize: "both",
+            backgroundSize: "cover",
+            overflow: "hidden",
+            ...(!minimalWidth && {
+                backgroundImage: "url(/solid-quest/images/background-lines.svg)",
+                backgroundPosition: "right top",
+            })
+        }}>
             <SessionProvider>
-
-                <Container maxWidth="lg" className={[styles.container, role].join(" ")}>
-
+                <Container maxWidth="lg" sx={{
+                    maxWidth: "60rem",
+                    padding: "1rem 0 3rem",
+                    margin: "0 auto",
+                    // minHeight: "97vh",
+                    width: "100%",
+                }}>
                     <Head>
                         <link rel="icon" href="/solid-quest/favicon.ico" />
                         <meta
@@ -52,9 +91,19 @@ export default function Layout({ children, home, role }: {
                         <meta name="og:title" content={siteTitle} />
                         <meta name="twitter:card" content="summary_large_image" />
                     </Head>
-                    <main>{children}</main>
 
-                    <Box className={styles.footer}  >
+                    <Box sx={{
+                        minHeight: "90vh",
+                        padding: "4rem 0",
+                        flex: 1,
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "center",
+                        alignItems: "center",
+
+                    }}>{children}</Box>
+
+                    <Box sx={footerStyle}  >
                         <Link href="/">Home</Link>
                         <Typography> | </Typography>
                         <Link href="https://labs.kadaster.nl/cases/Solid-Pods" target="_blank">
@@ -65,12 +114,12 @@ export default function Layout({ children, home, role }: {
                                 width={22}
                                 alt="Kadaster Labs"
                             />
-                             Labs
+                            Labs
                         </Link>
                     </Box>
                 </Container>
 
             </SessionProvider>
-        </Box>
+        </Box >
     );
 }
--- koopovereenkomst-v3-simple/src/PodIcon.tsx
+++ koopovereenkomst-v3-simple/src/PodIcon.tsx
@@ -10,11 +10,11 @@ export default function PodIcon(props: SvgIconProps) {
         xmlns="http://www.w3.org/2000/svg"
         fill="#000000"
       >
-        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+        <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
         <g
           id="SVGRepo_tracerCarrier"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         ></g>
         <g id="SVGRepo_iconCarrier">
           <path
