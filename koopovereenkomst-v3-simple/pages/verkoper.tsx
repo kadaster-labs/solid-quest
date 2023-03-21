e6988f0f381eccdb1adece37e23bf3d8721ea00f
Stefan Bussemaker <stefan.bussemaker@kadaster.nl>
2023-03-21 18:05:17 +0100

    feat: added v0.1 of step 4

--- koopovereenkomst-v3-simple/pages/verkoper.tsx
+++ koopovereenkomst-v3-simple/pages/verkoper.tsx
@@ -7,9 +7,10 @@ import Typography from "@mui/material/Typography";
 import Step1 from "./steps_verkoper/Step1";
 import Step2 from "./steps_verkoper/Step2";
 import Step3 from "./steps_verkoper/Step3";
+import Step4 from "./steps_verkoper/Step4";
+import Step5 from "./steps_verkoper/Step5";
 import Layout from "../src/Layout";
 import Head from "next/head";
-import Step4 from "./steps_verkoper/Step4";
 
 const steps = [
   "Datapod",
@@ -57,11 +58,11 @@ export default function Verkoper() {
       case 2:
         return <Step3 handleNext={handleNext} handleBack={handleBack} />;
       case 3:
-        return <Step4 step={activeStep} handleNext={handleNext} handleBack={handleBack} />;
+        return <Step4 handleNext={handleNext} handleBack={handleBack} />;
       case 4:
-        return <Step4 step={activeStep} handleNext={handleNext} handleBack={handleBack} />;
+        return <Step5  handleNext={handleNext} handleBack={handleBack} />;
       case 5:
-        return <Step4 step={activeStep} handleNext={() => { }} handleBack={handleBack} />;
+        return <Step5 handleNext={() => { }} handleBack={handleBack} />;
       default:
         return <Step1 handleNext={handleNext} />;
     }
