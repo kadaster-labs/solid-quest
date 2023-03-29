0d42290a07c3b195ac99f16d0b506e1188b395ba
Stefan Bussemaker <56066664+kad-busses@users.noreply.github.com>
2023-03-29 11:54:20 +0200

    Show VC verification status (#61)
    * feat: show verification result in step2
    
    * feat: download, refresh and delete VCs
    
    Works for step 2 now, step 3 is broken, will be fixed in next commit
    
    * fix: updated step 3 with new feature
    
    * refactor: move link viewing to VC comp
    
    * fix: downloadVC should check if file is available
    
    When saving a file to the solid pod, it isn't immediately available. For this reason, wait a bit after saving the VC to the pod until it is ready
    
    * feat: add loading icon when waiting for VC
    
    Sometimes takes a couple of seconds to properly persist in Solid Pod. Keep polling until the saved file has become available to load
    
    * refactor: some naming in VC saving

--- koopovereenkomst-v3-simple/pages/steps_verkoper/Step2.tsx
+++ koopovereenkomst-v3-simple/pages/steps_verkoper/Step2.tsx
@@ -3,17 +3,60 @@ import Typography from "@mui/material/Typography";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 import { Box } from "@mui/system";
-import VC, { VCType } from '../../src/VC';
+import VC, { SolidVC, VCType } from '../../src/VC';
 import Image from "../../src/Image";
-import { useState } from "react";
+import { useCallback, useState } from "react";
+
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+function createData(
+  name: string,
+  value: string,
+) {
+  return { name, value };
+}
 
 export default function Step2({ step = 2, handleNext, handleBack = () => { } }) {
-  const [vcLoaded, setvcLoaded] = useState(false);
+  const [loadedBRPVC, setLoadedBRPVC] = useState({} as any);
 
-  const handleVC = (_vc) => {
-    // get a trigger from <VC> to enable the "Doorgaan" button
-    setvcLoaded(true);
-  };
+  const [rows, setRows] = useState([] as Array<any>);
+
+  const updateVCs = useCallback(async (vcs: SolidVC[]) => {
+    // useCallback is important here. With each update from vcs
+    // <VC> will be re-rendered, and this will cause a new updateVCs
+    // resulting in an infinite loop.
+
+    if (vcs.length === 0) {
+      setLoadedBRPVC({});
+      return;
+    }
+
+    const vc = vcs[0];
+    setLoadedBRPVC(vc);
+
+    let certificateValidity;
+    // if type is string, it's a stringified JSON object
+    if (typeof vc.status === "string") {
+      certificateValidity = vc.status;
+    } else if (vc.status.verified) {
+      certificateValidity = "✅";
+    } else {
+      certificateValidity = "❌";
+    }
+
+    setRows([
+      createData('Naam', vc.vc.credentialSubject.naam),
+      createData('Geboortedatum', vc.vc.credentialSubject.geboorte.geboortedatum),
+      createData('Geboorteplaats', vc.vc.credentialSubject.geboorte.geboorteplaats),
+      createData('Geboorteland', vc.vc.credentialSubject.geboorte.geboorteland),
+      createData('Certificaat geldig?', certificateValidity),
+    ]);
+  }, []);
 
   return (
     <Box sx={{flex: 1}}>
@@ -24,21 +67,51 @@ export default function Step2({ step = 2, handleNext, handleBack = () => { } }) 
         {step}. Koppel je persoonsgegevens aan deze koopovereenkomst
       </Typography>
 
-      <Image
-        src="/solid-quest/images/mijnoverheid.png"
-        alt="Mijn Overheid Logo"
-        width={400}
-        height={180}
-        style={{ display: "block", margin: "25px auto" }}
-      />
+      <VC type={VCType.BRP} onChange={updateVCs}/>
 
       <hr/>
-
-      <VC type={VCType.BRP} handleVC={handleVC}/>
+      {Object.keys(loadedBRPVC).length === 0 ?
+      <Box>
+        <Image
+          src="/solid-quest/images/mijnoverheid.png"
+          alt="Mijn Overheid Logo"
+          width={400}
+          height={180}
+          style={{ display: "block", margin: "25px auto" }}
+        />
+      </Box>
+      :
+      <Box>
+        <Typography variant="body1" color="text.primary" align="center">
+          Je hebt je persoonsgegevens opgeslagen in je datakluis. Kloppen de gegevens?
+        </Typography>
+        {/* https://mui.com/material-ui/react-table/ */}
+        <TableContainer sx={{ marginY: '3rem' }} component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow
+                  key={row.name}
+                  sx={{ '&:last-child td, &:last-child th': {
+                    border: 0,
+                    fontStyle: 'italic',
+                    fontWeight: 'bold',
+                    height: '4rem',
+                  } }}
+                >
+                  <TableCell>{row.name}</TableCell>
+                  <TableCell>{row.value}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+      }
 
       <Stack direction="row" justifyContent="space-between">
         <Button variant="contained" onClick={handleBack}>Terug</Button>
-        {vcLoaded && <Button variant="contained" onClick={handleNext}>Doorgaan</Button> }
+        {Object.keys(loadedBRPVC).length !== 0 && <Button variant="contained" onClick={handleNext}>Doorgaan</Button> }
       </Stack>
     </Box>
   );
