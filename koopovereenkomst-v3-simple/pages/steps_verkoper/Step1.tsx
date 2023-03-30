dfc7be4cc51248ffef3152c117aa9a24b7f1c4b9
Stefan Bussemaker <stefan.bussemaker@kadaster.nl>
2023-03-30 13:38:16 +0200

    feat: login with own pod
    People interested in the demo can now log in using their own Pod. Since we only have Vera and Koos data available in the backend, send a request to the backend to add dummy data for the new webId. In Step 1, get the data from the Solid Pod and use the publicly available data to bootstrap some demo data

--- koopovereenkomst-v3-simple/pages/steps_verkoper/Step1.tsx
+++ koopovereenkomst-v3-simple/pages/steps_verkoper/Step1.tsx
@@ -1,19 +1,105 @@
 import { useSession } from "@inrupt/solid-ui-react";
+import {
+  getDate,
+  getSolidDataset,
+  getStringNoLocale,
+  getThing,
+  getUrl,
+  ThingPersisted,
+} from "@inrupt/solid-client";
 import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import { useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import ConnectSolid from "../../src/ConnectSolid";
+import { checkIfWebIDIsReady, registerWebID } from "../../src/mosService";
 
-export default function Step1({ handleNext, handleBack = () => { } }) {
+import { VCARD } from "@inrupt/vocab-common-rdf";
+import { SolidPerson, SolidAddress } from "../../src/SolidInterfaces";
 
+export default function Step1({ handleNext, handleBack = () => {} }) {
   const { session } = useSession();
 
+  const [isReady, setIsReady] = useState(null as boolean);
+  const [person, setPerson] = useState(null as SolidPerson);
+  const [eigendom, setEigendom] = useState(null as SolidAddress);
+
   let isLoggedIn = session.info.isLoggedIn;
+  let webId = session.info.webId;
+
+  const getProfile: () => Promise<ThingPersisted> = useCallback(async () => {
+    const myDataset = await getSolidDataset(webId, { fetch: session.fetch });
+    const profile: ThingPersisted = getThing(myDataset, webId);
+
+    return profile;
+  }, [webId, session.fetch]);
+
+  const getPersonInfo: (profile: ThingPersisted) => SolidPerson = useCallback(
+    (profile) => {
+      const name = getStringNoLocale(profile, VCARD.fn);
+      const bday = getDate(profile, VCARD.bday);
+
+      return { name, bday };
+    },
+    []
+  );
+
+  const getEigendomInfo: (profile: ThingPersisted) => Promise<SolidAddress> =
+    useCallback(
+      async (profile) => {
+        // Get the URL of the address, it is a different dataset than the profile (#me)
+        const addressUrl = getUrl(profile, VCARD.hasAddress);
+
+        const addressDataset = await getSolidDataset(addressUrl, {
+          fetch: session.fetch,
+        });
+        const address = getThing(addressDataset, addressUrl);
+
+        const streetAddress = getStringNoLocale(address, VCARD.street_address);
+        const locality = getStringNoLocale(address, VCARD.locality);
+        const region = getStringNoLocale(address, VCARD.region);
+        const postalCode = getStringNoLocale(address, VCARD.postal_code);
+        const countryName = getStringNoLocale(address, VCARD.country_name);
+
+        return { streetAddress, locality, region, postalCode, countryName };
+      },
+      [session.fetch]
+    );
+
+  // Na inloggen, check of het WebID bekend is in de database
+  useEffect(() => {
+    async function checkIfWebIDIsReadyForDemo(webId): Promise<void> {
+      const result = await checkIfWebIDIsReady(webId);
+      setIsReady(result);
+    }
+
+    async function loadData(): Promise<void> {
+      const profile = await getProfile();
+
+      const person = getPersonInfo(profile);
+      const eigendom = await getEigendomInfo(profile);
+
+      setPerson(person);
+      setEigendom(eigendom);
+    }
+
+    if (isLoggedIn) {
+      const result = checkIfWebIDIsReadyForDemo(webId);
+      if (result) {
+        loadData();
+      }
+    }
+  }, [getEigendomInfo, getPersonInfo, getProfile, isLoggedIn, webId]);
+
+  const registerWebIDForDemo: () => Promise<void> = async () => {
+    await registerWebID(webId, person, eigendom);
+    const result = await checkIfWebIDIsReady(webId);
+    setIsReady(result);
+  };
 
   return (
-    <Box sx={{flex: 1}}>
+    <Box sx={{ flex: 1 }}>
       <Typography variant="h1" color="text.primary" align="center">
         Start een nieuwe koopovereenkomst
       </Typography>
@@ -29,11 +115,44 @@ export default function Step1({ handleNext, handleBack = () => { } }) {
         <ConnectSolid />
       </Box>
 
-      { isLoggedIn &&
-      <Stack direction="row" justifyContent="end">
-        <Button disabled={!isLoggedIn} variant="contained" onClick={handleNext}>Doorgaan</Button>
-      </Stack>
-      }
+      {isLoggedIn && (
+        <Box>
+          <Typography variant="body1" color="text.primary" align="center">
+            Welkom{person.name !== null && " " + person.name}!
+          </Typography>
+          <pre>
+            {JSON.stringify(person, null, 2)}
+          </pre>
+          <pre>
+            {JSON.stringify(eigendom, null, 2)}
+          </pre>
+        </Box>
+      )}
+      {isLoggedIn && isReady == true && (
+        <Box>
+          <Stack direction="row" justifyContent="end">
+            <Button
+              disabled={!isLoggedIn}
+              variant="contained"
+              onClick={handleNext}
+            >
+              Doorgaan
+            </Button>
+          </Stack>
+        </Box>
+      )}
+      {isLoggedIn && isReady == false && (
+        <Box>
+          <Typography variant="body1" color="text.primary" align="center">
+            Nog niet klaar om te beginnen
+          </Typography>
+          <Stack direction="row" justifyContent="end">
+            <Button variant="contained" onClick={registerWebIDForDemo}>
+              Data klaarzetten
+            </Button>
+          </Stack>
+        </Box>
+      )}
     </Box>
   );
 }
---
+++ koopovereenkomst-v3-simple/src/SolidInterfaces.ts
@@ -0,0 +1,15 @@
+
+export interface SolidPerson {
+    name: string;
+    bday: Date;
+}
+
+export interface SolidAddress {
+    streetAddress: string;
+    locality: string;
+    region: string;
+    postalCode: string;
+    countryName: string;
+}
+
+// Hier komt de overige Solid functionaliteit
