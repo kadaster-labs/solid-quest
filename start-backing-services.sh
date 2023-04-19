6d4837367c80dcefe6e2b87ece97ad89fe9995c7
Marc van Andel <marc.vanandel@kadaster.nl>
2023-04-19 07:40:08 +0200

    moved build to flag in start script

--- start-backing-services.sh
+++ start-backing-services.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cli_command='docker compose'
+build_flag=''
 
 function podman_safety_check() {
     if ! command -v docker &> /dev/null
@@ -17,6 +18,15 @@ function podman_safety_check() {
 
 podman_safety_check
 
+OPTION_BUILD=$1
+if [[ "$OPTION_BUILD" == "-b" ]] 
+then
+    echo "Building images before starting them ..."
+    echo
+    echo "Setting the build flag [--build]"
+    build_flag='--build'
+fi
+
 echo "Starting backing services for development ..."
 echo
-$cli_command up solid-pod-provider mock-overheid-server vc-api
+$cli_command up solid-pod-provider mock-overheid-server vc-api $build_flag
